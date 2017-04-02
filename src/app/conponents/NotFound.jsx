5daa47de7a799a7db585fbd29ff09fd256ddea2e
Maksim Kostromin <daggerok@gmail.com>
2017-04-02 12:16:59 +0300

    add react-redux connect decorator

--- src/app/conponents/NotFound.jsx
+++ src/app/conponents/NotFound.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
-const StatelessNotFound = props => <div>
-  <h1>=(</h1>
-  <div>
-    Sorry, but page with path <code>'{props.location.pathname}'</code> was not found...
-  </div>
-  <code>component props</code>
-  <pre><code>{JSON.stringify(props, null, 2)}</code></pre>
-</div>;
-
-export const StatefulNotFound = connect(
-  state => (state, ownProps) => (Object.assign({}, ownProps, { counter: state.first }))
-)(StatelessNotFound);
+// to able using decorators @decoratorName you need to
+// yarn add -ED babel-plugin-transform-decorators-legacy
+// add plugins: [ "transform-decorators-legacy" ] to .babelrc
+@connect(
+  state => (state, ownProps) => (
+    {
+      ...ownProps,
+      counter: state.first,
+    }
+  )
+)
+export class StatefulNotFound extends Component {
+  render() {
+    const { location } = this.props;
+    return (
+      <div>
+        <h1>=(</h1>
+        <div>
+          Sorry, but page with path <code>'{location.pathname}'</code> was not found...
+        </div>
+        <code>component props</code>
+        <pre><code>{JSON.stringify(this.props, null, 2)}</code></pre>
+      </div>
+    );
+  }
+}
